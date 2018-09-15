6ad096ea1a74fd09831791644b49b02b2318af58
Baojian Zhou <bzhou6@albany.edu>
2018-09-15 00:38:02 -0400

    dd

---
+++ chapter02/example_01.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+print(np.float32)
+x = 1.0
+print(np.finfo(np.float32).max)
+print(np.finfo(np.float32).min)
+print(np.finfo(np.float64).max)
+print(np.finfo(np.float64).min)
+print(np.finfo(np.float32).eps)
+print(np.finfo(np.float64).eps)
